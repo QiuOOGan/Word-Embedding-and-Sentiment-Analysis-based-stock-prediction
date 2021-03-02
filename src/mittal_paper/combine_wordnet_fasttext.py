5c812937b2dc3d14e5f1a225e4cdaa2efbeca101
QiuOOGan <ganqiu@ucdavis.edu>
2021-03-02 14:18:07 -0600

    combine

---
+++ src/mittal_paper/combine_wordnet_fasttext.py
@@ -0,0 +1,18 @@
+import json
+
+f1 = open('syn_to_POMS_wordnet.json')
+SYN_TO_POMS = json.load(f1)
+f2 = open('syn_to_POMS_fasttext.json')
+SYN_TO_POMS2 = json.load(f2)
+
+
+for word in SYN_TO_POMS2:
+    if word not in SYN_TO_POMS:
+        SYN_TO_POMS[word] = SYN_TO_POMS2[word]
+
+print(SYN_TO_POMS)
+with open('syn_to_POMS_combined.json', 'w') as fp:
+    json.dump(SYN_TO_POMS, fp, sort_keys=True, indent=4)
+
+
+
